439f763b143ca6f810adc33c7512e11cdd149069
Agung Firmansyah <agungfirid@gmail.com>
2022-04-08 23:25:28 +0700

    first init

---
+++ app/src/main/java/com/agungfir/intermediateandroiddicoding/MyButton.kt
@@ -0,0 +1,69 @@
+package com.agungfir.intermediateandroiddicoding
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+
+class MyButton : AppCompatButton, View.OnTouchListener {
+
+    private lateinit var enabledBackground: Drawable
+    private lateinit var disabledBackground: Drawable
+    private var txtColor: Int = 0
+
+    constructor(context: Context) : super(context) {
+        init()
+        setOnTouchListener(this)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+        setOnTouchListener(this)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+        setOnTouchListener(this)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        background = if (isEnabled) enabledBackground else disabledBackground
+        setTextColor(txtColor)
+        textSize = 12f
+        gravity = Gravity.CENTER
+//        text = if (isEnabled) "Submit" else "Isi Dulu"
+    }
+
+    private fun init() {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
+        enabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button) as Drawable
+        disabledBackground =
+            ContextCompat.getDrawable(context, R.drawable.bg_button_disable) as Drawable
+    }
+
+    override fun onTouch(view: View, event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                text = "Aku Tertekan"
+                return true
+            }
+            MotionEvent.ACTION_UP -> {
+                text = "Aku Dilepas"
+                return true
+            }
+        }
+
+        return false
+
+    }
+}
